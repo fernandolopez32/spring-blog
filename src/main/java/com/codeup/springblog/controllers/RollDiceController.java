fa1727a986efcfb6e3827e466d2af9d6e07b46b7
Fernando Lopez <flopez00032@gmail.com>
2022-11-17 09:49:25 -0600

    before lecture

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
@@ -12,5 +13,7 @@ public class RollDiceController {
     public String welcome(){
         return "roll-dice";
     }
+
+
 
 }
--- src/main/java/com/codeup/springblog/models/Dice.java
+++ src/main/java/com/codeup/springblog/models/Dice.java
@@ -1,4 +1,10 @@
 package com.codeup.springblog.models;
 
 public class Dice {
+
+    final int sides = 6;
+
+    public int roll(){
+        return (int) (Math.random()*sides) +1;
+    }
 }
