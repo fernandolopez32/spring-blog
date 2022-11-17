aaefaebaabfc7aa5049ed841d147f9afe81f3ed2
Fernando Lopez <flopez00032@gmail.com>
2022-11-17 16:58:57 -0600

    finished dice

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,23 +1,38 @@
 package com.codeup.springblog.controllers;
 
 
+import com.codeup.springblog.models.Dice;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/roll-dice")
 public class RollDiceController {
 
-    @GetMapping("/{number}")
+    @GetMapping
     public String welcome(){
         return "roll-dice";
     }
 
+    @GetMapping("/{number}")
+    @ResponseBody
+    public String guess(@PathVariable int number, Model model) {
+        Dice dice = new Dice();
 
+        int randNum = dice.roll();
 
+        model.addAttribute("select", dice);
+        if (randNum == number) {
+            return "you won";
+        } else {
+            return "you lost";
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println();
+    }
 
 
 }
--- src/main/java/com/codeup/springblog/models/Dice.java
+++ src/main/java/com/codeup/springblog/models/Dice.java
@@ -4,7 +4,17 @@ public class Dice {
 
     final int sides = 6;
 
+//    private int number;
+
     public int roll(){
         return (int) (Math.random()*sides) +1;
     }
+//
+//    public int getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(int number) {
+//        this.number = number;
+//    }
 }
