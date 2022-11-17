0cfaefcbd0f88a334cf69403e8205d9f5d69bf2d
Fernando Lopez <flopez00032@gmail.com>
2022-11-17 15:39:07 -0600

    before stopping for presentation

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -4,15 +4,19 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/roll-dice")
 public class RollDiceController {
 
-    @GetMapping("/roll-dice")
+    @GetMapping("/{number}")
     public String welcome(){
         return "roll-dice";
     }
+
+
+
 
 
 
