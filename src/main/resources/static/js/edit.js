90c6a9e5d79190261732c526ac7686d9edbbf1dd
Fernando Lopez <flopez00032@gmail.com>
2022-12-01 21:09:35 -0600

    jquerry wont work and im getting no errors

--- src/main/resources/static/js/edit.js
+++ src/main/resources/static/js/edit.js
@@ -1,12 +1,20 @@
-$(function(){
+// $(function(){
+//     alert("yo");
+//
+//     $(".edit-button").on("click",function (){
+//         console.log($(this).attr("data-id"));
+//         window.location.replace(`/post/${$(this).attr("data-id")}/edit`);
+//     });
+//
+//
+// });
+
+$(document).ready(function (){
+
     alert("yo");
 
     $(".edit-button").on("click",function (){
         console.log($(this).attr("data-id"));
         window.location.replace(`/post/${$(this).attr("data-id")}/edit`);
-    })
-
-    $(".editButton").on('click', function(e){
-        window.location.replace(`/posts/${$(this).attr("data-id")}/edit`);
     });
-})
+});
